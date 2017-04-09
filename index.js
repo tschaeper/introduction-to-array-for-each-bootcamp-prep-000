da374c5b231eaa9369d5ae9b45068fbd2f060893
Tom Schaeper <schaeper@hyperverse.com>
2017-04-09 00:01:28 +0000

    Done.

--- index.js
+++ index.js
@@ -4,8 +4,30 @@ for (var i = 0, l = evens.length; i < l; i++) {
   console.log(`${evens[i]} is not odd!`)
 }
 
-var evens = [0, 2, 4, 6, 8, 10]
- 
 evens.forEach(even => {
   console.log(`${even} is not odd!`)
 })
+
+function square(n) {
+  console.log(n * n)
+}
+
+function doToEvens(callback) {
+  evens.forEach(callback)
+}
+
+function doToElementsInArray(array, callback) {
+  array.forEach(callback)
+}
+
+function changeCompletely(element, index, array) {
+  array[index] = (Math.random() * 100).toString() + '!!!'
+}
+ 
+var animals = ["dog", "fish", "cat"]
+ 
+doToElementsInArray(animals, changeCompletely)
+ 
+// log out animals -- pretty cool, right?
+console.log(animals)
+
